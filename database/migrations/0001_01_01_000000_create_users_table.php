c8945b5ffdcc6ecb2e0c7e017d7d225b6edeb9ab
laurogomes1 <laurogomes1@gmail.com>
2024-09-13 14:06:19 -0300

    Primeiro commit

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name'); // Campo first_name
+            $table->string('last_name');  // Campo last_name
+            $table->string('email')->unique(); // Campo email padrão
+            $table->timestamp('email_verified_at')->nullable(); // Verificação de email
+            $table->string('password'); // Campo password
+            $table->string('whatsapp'); // Campo whatsapp
+            $table->rememberToken(); // Token padrão para "lembrar-me"
+            $table->timestamps(); // Cria 'created_at' e 'updated_at' automaticamente
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
